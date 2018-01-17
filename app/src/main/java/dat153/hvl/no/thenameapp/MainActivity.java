0bc39a2023918835c988447b903d427b1c1456e2
stephanie94 <stephanie@simba.no>
2018-01-17 13:50:45 +0100

    :)

--- app/src/main/java/dat153/hvl/no/thenameapp/MainActivity.java
+++ app/src/main/java/dat153/hvl/no/thenameapp/MainActivity.java
@@ -5,14 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static HashMap<String, Integer> mStudentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mStudentList = new HashMap<>();
+        initiateStudentList();
     }
 
+    public void initiateStudentList() {
+        mStudentList.put("Stephanie Marthinussen", R.drawable.rabbit1);
+        mStudentList.put("Magnus Marthinsen", R.drawable.rabbit4);
+        mStudentList.put("Adrian Storm-Johannessen", R.drawable.rabbit3);
+    }
     public void loadNameListMode(View v){
         Intent startNewActivity = new Intent(this, NameListActivity.class);
         startActivity(startNewActivity);
--- app/src/main/java/dat153/hvl/no/thenameapp/NameListActivity.java
+++ app/src/main/java/dat153/hvl/no/thenameapp/NameListActivity.java
@@ -2,12 +2,40 @@ package dat153.hvl.no.thenameapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class NameListActivity extends AppCompatActivity {
+    private List<String> list;
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //TODO : Create new view
+        setContentView(R.layout.activity_name_list); //TODO : Create new view
+
+        list = new ArrayList<String>();
+        ListView listView = (ListView) findViewById(R.id.list_View);
+
+        HashMap<String, Integer> studentList = MainActivity.mStudentList;
+        final String[] names = new String[studentList.size()];
+        int i = 0;
+        for(String s : studentList.keySet()) {
+            names[i] = s;
+            i++;
+        }
+
+        final List<String> listNames = new ArrayList<>(Arrays.asList(names));
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listNames);
+
+        listView.setAdapter(adapter);
+
+
     }
 
 }

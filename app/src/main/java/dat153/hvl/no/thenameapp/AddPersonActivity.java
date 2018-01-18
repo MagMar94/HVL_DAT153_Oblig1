b93a256d7bebba74593f425b153b39e4905a0e5d
stephanie94 <stephanie@simba.no>
2018-01-18 11:18:50 +0100

    Knapp som lager ny aktivitet som skal brukes til å legge til nye personer

---
+++ app/src/main/java/dat153/hvl/no/thenameapp/AddPersonActivity.java
@@ -0,0 +1,20 @@
+package dat153.hvl.no.thenameapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by stephanie on 18.01.2018.
+ */
+
+public class AddPersonActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
--- app/src/main/java/dat153/hvl/no/thenameapp/NameListActivity.java
+++ app/src/main/java/dat153/hvl/no/thenameapp/NameListActivity.java
@@ -1,7 +1,9 @@
 package dat153.hvl.no.thenameapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -35,7 +37,11 @@ public class NameListActivity extends AppCompatActivity {
 
         listView.setAdapter(adapter);
 
+    }
 
+    public void loadAddPersonAcitivity(View v) {
+        Intent startNewActivity = new Intent(this, AddPersonActivity.class);
+        startActivity(startNewActivity);
     }
 
 }

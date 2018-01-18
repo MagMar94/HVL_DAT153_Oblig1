b6d51f4f29ffbf8567f85dc632b48ac7869a887e
Magnus Marthinsen <magmar@online.no>
2018-01-18 10:46:14 +0100

    Flyttet all dataen ut i egen klasse (singleton). Byttet om keys og values, fordi bildene alltid vil være unike mens flere kan hete det samme.

--- app/src/main/java/dat153/hvl/no/thenameapp/MainActivity.java
+++ app/src/main/java/dat153/hvl/no/thenameapp/MainActivity.java
@@ -9,21 +9,12 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static HashMap<String, Integer> mStudentList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mStudentList = new HashMap<>();
-        initiateStudentList();
     }
 
-    public void initiateStudentList() {
-        mStudentList.put("Stephanie Marthinussen", R.drawable.rabbit1);
-        mStudentList.put("Magnus Marthinsen", R.drawable.rabbit4);
-        mStudentList.put("Adrian Storm-Johannessen", R.drawable.rabbit3);
-    }
     public void loadNameListMode(View v){
         Intent startNewActivity = new Intent(this, NameListActivity.class);
         startActivity(startNewActivity);

dd90ac1daa4cf1836203c4bfeb8a910230232169
stephanie94 <stephanie@simba.no>
2018-01-18 13:34:43 +0100

    Prøvd å fikse så bildet som blir tatt kan vises.

--- app/src/main/java/dat153/hvl/no/thenameapp/AddPersonActivity.java
+++ app/src/main/java/dat153/hvl/no/thenameapp/AddPersonActivity.java
@@ -1,7 +1,11 @@
 package dat153.hvl.no.thenameapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 /**
  * Created by stephanie on 18.01.2018.
@@ -13,7 +17,24 @@ public class AddPersonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView pictureOfPersonView = (ImageView) findViewById(R.id.imageView);
+            pictureOfPersonView.setImageBitmap(imageBitmap);
+        }
+    }
 
 
 

e4d18c58ffa498eeaf3120c418152c36627ba8a7
Magnus Marthinsen <magmar@online.no>
2018-01-19 10:20:46 +0100

    Gjort så ta bilde-knappen fungerer. Bildet er veldig mye mørkere enn originalen på min skjerm, men kanskje ikke på andres?

--- app/src/main/java/dat153/hvl/no/thenameapp/AddPersonActivity.java
+++ app/src/main/java/dat153/hvl/no/thenameapp/AddPersonActivity.java
@@ -5,21 +5,29 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 /**
  * Created by stephanie on 18.01.2018.
+ *
+ * @see <a href="https://developer.android.com/training/camera/photobasics.html#TaskPath">developer.android.com/...</a>
  */
-
 public class AddPersonActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_person);
+        //takePictureOfPerson();
     }
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    private void dispatchTakePictureIntent() {
+    /**
+     * Fixes so an image is captured if another application on the device can handle the event.
+     */
+    public void takePictureOfPerson(View v) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -28,10 +36,11 @@ public class AddPersonActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView pictureOfPersonView = (ImageView) findViewById(R.id.imageView);
+            ImageView pictureOfPersonView = findViewById(R.id.imageView);
             pictureOfPersonView.setImageBitmap(imageBitmap);
         }
     }

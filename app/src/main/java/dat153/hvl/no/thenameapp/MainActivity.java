818cf7c896c8225cfc4f90f0d68d4cae0c7cc083
Magnus Marthinsen <magmar@online.no>
2018-01-19 13:38:59 +0100

    Gjort så ikke navnene blir lagt inn i HashMapped flere ganger.

--- app/src/main/java/dat153/hvl/no/thenameapp/MainActivity.java
+++ app/src/main/java/dat153/hvl/no/thenameapp/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initiateStudentList();
+        if(People.mInstance.mPeopleMap.isEmpty())
+            initiateStudentList();
     }
 
     private void initiateStudentList() {

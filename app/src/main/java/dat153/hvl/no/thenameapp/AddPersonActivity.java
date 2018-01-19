fcd7f19025a985c593c664f36c58aed3567f2f9f
Magnus Marthinsen <magmar@online.no>
2018-01-19 14:56:21 +0100

    Tror ikke denne filen har blitt med til Git.

--- app/src/main/java/dat153/hvl/no/thenameapp/AddPersonActivity.java
+++ app/src/main/java/dat153/hvl/no/thenameapp/AddPersonActivity.java
@@ -35,7 +35,6 @@ public class AddPersonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_person);
-        //takePictureOfPerson();
     }
 
     /**

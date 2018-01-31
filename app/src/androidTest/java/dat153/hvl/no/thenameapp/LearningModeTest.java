f6022184c7bb706b670c21152e28c335cf2160d1
Magnus Marthinsen <magmar@online.no>
2018-01-31 14:17:41 +0100

    Fikset en app context som lager contexten. Det skal gjøre slik at vi initsialiserer listen med studenter utneom aktivitetene. Dette skal gjøre det mulig å launce learning mode aktiviteten direkte.

---
+++ app/src/androidTest/java/dat153/hvl/no/thenameapp/LearningModeTest.java
@@ -0,0 +1,22 @@
+package dat153.hvl.no.thenameapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Magnus on 31.01.2018.
+ */
+@RunWith(AndroidJUnit4.class)
+public class LearningModeTest {
+    @Rule
+    public ActivityTestRule<LearningModeActivity> mActivityRule = new ActivityTestRule<LearningModeActivity>(LearningModeActivity.class);
+
+    @Test
+    public void activityStarts(){
+
+    }
+}

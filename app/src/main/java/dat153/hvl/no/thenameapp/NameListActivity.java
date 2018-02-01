d8abc72f334afaa7b1fc3f82bc26b3a8aeb32018
Magnus Marthinsen <magmar@online.no>
2018-02-01 12:35:45 +0100

    Fikset så tilbakeknappen fra NameListActivity går til hovedmenyen eller viser listen avhengig av hva som er på skjermen. Bruker en tilstandsmaskin for å oppnå dette. Var litt slem og fikset en urelatert skrivefeil i samme commit.

--- app/src/main/java/dat153/hvl/no/thenameapp/NameListActivity.java
+++ app/src/main/java/dat153/hvl/no/thenameapp/NameListActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,12 +15,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NameListActivity extends AppCompatActivity {
+    private enum DisplayedLayout {LIST, PERSON};
+    private DisplayedLayout displayedLayout;
+
     private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_list);
+        displayedLayout = DisplayedLayout.LIST;
     }
 
     @Override
@@ -27,7 +34,26 @@ public class NameListActivity extends AppCompatActivity {
         fillList();
     }
 
-    public void loadAddPersonAcitivity(View v) {
+    /**
+     * Dette er fordi view-et endres til å vise personen, og da vil vi at tilbakeknappen skal
+     * gå tilbake til listen.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            switch (displayedLayout){
+                case LIST:
+                    super.onBackPressed();
+                case PERSON:
+                    setContentView(R.layout.activity_name_list);
+                    fillList();
+                    displayedLayout = DisplayedLayout.LIST;
+            }
+        }
+        return true;
+    }
+
+    public void loadAddPersonActivity(View v) {
         Intent startNewActivity = new Intent(this, AddPersonActivity.class);
         startActivity(startNewActivity);
     }
@@ -42,6 +68,7 @@ public class NameListActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 setTitle(People.mInstance.mPeopleMap.values().toArray()[i].toString());
                 setContentView(R.layout.activity_show_picture);
+                displayedLayout = DisplayedLayout.PERSON;
                 ImageView iw = findViewById(R.id.displayPersonImage);
                 BitmapDrawable image = (BitmapDrawable) People.mInstance.mPeopleMap.keySet().toArray()[i];
                 iw.setImageDrawable(image);
